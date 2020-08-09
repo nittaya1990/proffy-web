73096c90324f3de38a3bcfc345a682fbcebf0ee3
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2020-08-08 21:29:51 -0300

    connected web with api.

--- src/components/TeacherIten/index.tsx
+++ src/components/TeacherIten/index.tsx
@@ -2,33 +2,55 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+    id: number
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;    
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+function createNewConnection(){
+    api.post('connections', {
+        user_id: teacher.id
+    });
+}
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/49327237?s=400&u=3b9ac2cb488cf243d35c0753001d1f7250f7f474&v=4" alt="Marcos Winicios"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Marcos Winicios</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
                 </header>
 
-                <p>
-                    Entusiasta das melhores tecnologias de química avançada.
-                    <br /><br />
-                    Apaixonado por explodir coisas em laboratório e por mudar a vidas as pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                </p>
+                    <p>{teacher.bio}</p>
 
                 <footer>
                     <p>
                         Preço/hora
-                        <strong>R$ 80,00</strong>
+                        <strong>R$ {teacher.cost}</strong>
                     </p>
-                    <button type="button">
+                    <a 
+                        target="_blank" 
+                        onClick={createNewConnection} 
+                        href={`https://wa.me/${teacher.whatsapp}?text=Olá professor ${teacher.name}! encontrei seu contato na plataforma Proffy sobre as aulas de ${teacher.subject}, gostaria de receber mais informações.`}
+                        type="button"
+                    >
                         <img src={whatsappIcon} alt="Whatsapp"/>
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
             
         </article>
